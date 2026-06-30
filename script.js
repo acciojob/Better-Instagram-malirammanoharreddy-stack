573d9884c7953b470ebd1108ce1d4b87bce511df
RAM MANOHAR REDDY <malirammanoharreddy@gmail.com>
2026-06-30 12:58:49 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
 //your code here
+const parent = document.getElementById("parent");
+const images = document.querySelectorAll(".image");
+images.forEach(image => {
+	image.addEventListener("dragstart", function (e) {
+		e.dataTransfer.setData("text", e.target.id);
+	})
+
+	image.addEventListener("dragover", function(e) {
+		e.preventDefault();
+	})
+
+	image.addEventListener("drop", function (e) {
+		e.preventDefault();
+		const dragId = e.dataTransfer.getData("text");
+		const dragElement = document.getElementById(dragId);
+		const nextToDragEle = dragElement.nextElementSibling;
+		parent.insertBefore(dragElement, image);
+		if(nextToDragEle){
+			parent.insertBefore(image, nextToDragEle);
+		} else {
+			parent.append(image);
+		}
+	})
+})
